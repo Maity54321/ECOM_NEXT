44af261b093770645c99a1d56f7cad99da277cdf
Maity54321 <maitysurodoy@gmail.com>
2026-05-01 22:21:20 +0530

    Changed API URL

--- services/link.js
+++ services/link.js
@@ -2,4 +2,4 @@ console.log(process.env.NEXT_PUBLIC_MYENV);
 export const APIUrl =
   process.env.NEXT_PUBLIC_MYENV === "prod"
     ? "https://mern-ecom-vercel.vercel.app"
-    : "http://localhost:4000";
+    : "https://mern-ecom-vercel.vercel.app";
